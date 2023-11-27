c18a1e9dcb2d8d8e54061dee2cdc6019c456b05a
CarrieJiangkang <109514802+CarrieJiangkang@users.noreply.github.com>
2023-11-27 10:53:23 +0800

    Update demo.m

--- code/demo.m
+++ code/demo.m
@@ -1,11 +1,1 @@
-X = y';
-Y = MDS_LogP(:,end); 
-[beta,resnorm] = fit_nonlinear_data(X,Y);
 
-
-% beta = zeros(1,37);
-% beta(0) = zeros(1,37);
-% for s=1:37
-%      beta_s = beta(s);
-%      beta = [beta beta_s];
-% end
